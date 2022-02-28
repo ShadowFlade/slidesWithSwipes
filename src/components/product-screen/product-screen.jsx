a4695bb0f0bbc6b3c4ab1384132de5615c350c6c
ShadowFlade <Wolffdrugov@yandex.ru>
2022-02-28 12:31:24 +0300

    added bubbles

--- src/components/product-screen/product-screen.jsx
+++ src/components/product-screen/product-screen.jsx
@@ -3,7 +3,7 @@ import MessageWindow from '../message-window/message-window';
 import bg from './bg3.png';
 import product from './product.png';
 import darkPinkBubble from './dark-pink-bubble.png';
-import lightBlueBubble from './dark-pink-bubble.png';
+import lightBlueBubble from './light-blue.png';
 import AnimatedElement from '../animated-element/animated-element';
 import { wobbleAnimation } from '../../animations/animations';
 // import bubble from './dark-pink-bubble.png';
@@ -28,16 +28,75 @@ export default function ProductScreen({ setBackgroundPic }) {
           <div className="product-screen__product">
             <img src={product} alt="seems like a bottle of soup" />
             <AnimatedElement
-              x={0}
-              y={0}
+              x={50}
+              y={150}
               src={darkPinkBubble}
               alt="dark pink bubble"
               animation={wobbleAnimation}
-              width={100}
-              height={80}
-              offset={20}></AnimatedElement>
-            {/* <AnimatedElement></AnimatedElement>
-<AnimatedElement></AnimatedElement> */}
+              width={30}
+              height={30}
+              offset={20}
+              style={{ zIndex: '2' }}></AnimatedElement>
+            <AnimatedElement
+              x={50}
+              y={10}
+              src={lightBlueBubble}
+              alt="light blue bubble"
+              animation={wobbleAnimation}
+              width={45}
+              height={45}
+              offset={20}
+              style={{ zIndex: '4' }}></AnimatedElement>
+            <AnimatedElement
+              x={50}
+              y={200}
+              src={lightBlueBubble}
+              alt="light blue bubble"
+              animation={wobbleAnimation}
+              width={45}
+              height={45}
+              offset={20}
+              style={{ zIndex: '4' }}></AnimatedElement>
+            <AnimatedElement
+              x={100}
+              y={250}
+              src={lightBlueBubble}
+              alt="light blue bubble"
+              animation={wobbleAnimation}
+              width={45}
+              height={45}
+              offset={20}
+              style={{ zIndex: '4' }}></AnimatedElement>
+            <AnimatedElement
+              x={50}
+              y={300}
+              src={lightBlueBubble}
+              alt="light blue bubble"
+              animation={wobbleAnimation}
+              width={45}
+              height={45}
+              offset={20}
+              style={{ zIndex: '5' }}></AnimatedElement>
+            <AnimatedElement
+              x={35}
+              y={265}
+              src={darkPinkBubble}
+              alt="light blue bubble"
+              animation={wobbleAnimation}
+              width={45}
+              height={45}
+              offset={20}
+              style={{ zIndex: '5' }}></AnimatedElement>
+            <AnimatedElement
+              x={0}
+              y={120}
+              src={darkPinkBubble}
+              alt="light blue bubble"
+              animation={wobbleAnimation}
+              width={45}
+              height={45}
+              offset={20}
+              style={{ zIndex: '5' }}></AnimatedElement>
           </div>
           <MessageWindow items={items}></MessageWindow>
         </div>
